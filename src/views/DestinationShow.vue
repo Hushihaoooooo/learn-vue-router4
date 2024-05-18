f7fba1bfd9e7be6cc6d4feca8930d0742f540638
ShiHaoHu <2462833786@qq.com>
2024-05-18 12:18:17 +0800

    first play

--- src/views/DestinationShow.vue
+++ src/views/DestinationShow.vue
@@ -1,26 +1,46 @@
-<!-- <template>
-    <section class="destination">
-      <h1>{{destination.name}}</h1>
-      <div class="destination-details">
-        <img :src="`/images/${destination.image}`" :alt="destination.name">
-        <p>{{destination.description}}</p>
-      </div>
-    </section>
-  </template>
-  <scrip>
+<template>
+  <section class="destination">
+    <h1>{{ destination.name }}</h1>
+    <div class="destination-details">
+      <img :src="`/images/${destination.image}`" :alt="destination.name">
+      <p>{{ destination.description }}</p>
+    </div>
+  </section>
+  <section class="experiences">
+    <h2>Top experiences in {{ destination.name }}</h2>
+    <div class="cards">
+      <router-link 
+      v-for="experience in destination.experiences" 
+      :key="experience.slug"
+      :to="{name: 'experience.show', params:{experienceSlug: experience.slug}}"
+      >
+        <!-- 这里的:experience是传出的参数 -->
+        <ExperienceCard :experience="experience" />
+      </router-link>
+
+    </div>
+
+  </section>
+</template>
+<script>
   import sourceData from '@/data.json'
+  import ExperienceCard from '@/components/ExperienceCard.vue'
   export default {
+    components: {ExperienceCard},
+    props:{
+    id: {type: Number, required: true}
+    },
     computed:{
       destinationId(){
         return parseInt(this.$route.params.id)
       },
       destination(){
-        return sourceData.destinations.find(destination => destination.id === this.destinationId)
+        return sourceData.destinations.find(destination => destination.id === this.id)
       }
     }
   }
-  </script> -->
-<template>
+  </script>
+<!-- <template>
     <section class="destinations">
         <h1>{{ destination.name }}</h1>
         <div class="destination-details">
@@ -50,4 +70,4 @@ async function fetchDestination() {
 fetchDestination();
 
 watch(() => route.params, fetchDestination);
-</script>
+</script> -->
